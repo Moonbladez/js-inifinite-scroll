19eddd41bfcc2560d83954ef4e2b20ce4f3a6e64
Bekki Young <moonbladez@hotmail.com>
2020-04-28 13:55:19 +0200

    filter result functionality added

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const postContainer = document.querySelector(".post-container")
 const loading = document.querySelector(".loader")
 const filter = document.querySelector(".filter")
 
-//globals 
-let limit = 3;
+
+
+let limit = 5;
 let page = 1;
 
+// Fetch posts from API
+async function getPosts() {
+    const res = await fetch(
+        `http://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+    );
 
-//fetch post from API
-const getPosts = async () => {
-    const reponse = await fetch(`https://jsonplaceholder.typicode.com/users/1/posts?_limit=${limit}`);
-
-    const data = await reponse.json();
+    const data = await res.json();
 
     return data;
 }
@@ -21,7 +23,7 @@ const getPosts = async () => {
 //show posts in dom 
 const showPosts = async () => {
     const posts = await getPosts();
-
+    console.log(limit, page)
     posts.forEach(post => {
         const postElement = document.createElement("div")
         postElement.classList.add("post")
@@ -39,6 +41,56 @@ const showPosts = async () => {
     })
 }
 
+//show loading bar
+const showLoading = () => {
+    loading.classList.add("show")
 
 
+    setTimeout(() => {
+        loading.classList.remove('show');
+
+        setTimeout(() => {
+            page++;
+            showPosts();
+        }, 300);
+    }, 2500);
+}
+
+//filter posts
+const filterPosts = (event) => {
+    const searchTerm = event.target.value
+    const posts = document.querySelectorAll(".post")
+
+    posts.forEach(post => {
+        const title = post.querySelector(".post-title").innerText.toUpperCase();
+        const body = post.querySelector(".post-body").innerText.toUpperCase()
+
+        if (title.indexOf(searchTerm) > -1 || body.indexOf(searchTerm) > -1) {
+            post.style.display = "flex"
+        } else {
+            post.style.display = "none"
+        }
+    })
+}
+
+
+//show inital post
 showPosts()
+
+
+
+//listen for scroll height to show loader
+window.addEventListener("scroll", () => {
+    const {
+        scrollTop,
+        scrollHeight,
+        clientHeight
+    } = document.documentElement
+
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+        showLoading();
+    }
+
+})
+
+filter.addEventListener("input", filterPosts)
